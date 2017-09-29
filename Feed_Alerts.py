bff1c9669be837daea64ee480b6c9662f88f4c14
umeshdhanwal <31568819+umeshdhanwal@users.noreply.github.com>
2017-09-29 10:45:39 +0800

    Update Feed_Alerts.py

--- Feed_Alerts.py
+++ Feed_Alerts.py
@@ -1,1 +1,7 @@
-
+import feedparser
+d = feedparser.parse('https://www.google.com/alerts/feeds/14273445301609542014/5918640904071536250')
+for e in d.entries:
+     print(e.title)
+     print(e.link)
+     print(e.description)
+     print("\n") # 2 newlines
