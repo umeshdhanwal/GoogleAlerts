f4e2a92178078ae7e37e79018e8c6b55a96472db
umeshdhanwal <31568819+umeshdhanwal@users.noreply.github.com>
2017-09-30 14:22:51 +0800

    Update Final_Google_alerts.py

--- Final_Google_alerts.py
+++ Final_Google_alerts.py
@@ -64,7 +64,7 @@ class TransferData:
 print("The copiedName:",NameofFile)
 
 file_from=NameofFile
-file_to="\"+NameofFile
+file_to='\'+NameofFile
 
 transferData.upload_file(file_from, file_to)
 print(dbx.files_get_metadata( '/'+NameofFile).server_modified)
