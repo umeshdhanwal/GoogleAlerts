02803c534ac3bb7c60d2d4b4fa4218dd8648c9db
umeshdhanwal <31568819+umeshdhanwal@users.noreply.github.com>
2017-09-30 14:03:54 +0800

    Update Final_Google_alerts.py

--- Final_Google_alerts.py
+++ Final_Google_alerts.py
@@ -52,8 +52,8 @@ dbx = dropbox.Dropbox(app_secret)
 filetocopy=open('/home/umeshlives/GoogleAlerts/'+NameofFile,'w')
 print("The copiedName:",NameofFile)
 data = filetocopy.read()      # copy to a string
-filetocopy.close() 
 print(data)
 
 dbx.files_upload(data, '/'+NameofFile, mute=True)
 print(dbx.files_get_metadata( '/'+NameofFile).server_modified)
+filetocopy.close()
