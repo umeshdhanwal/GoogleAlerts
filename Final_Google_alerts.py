3532de64674df3be5c52725ca09d2249ccedfc1d
umeshdhanwal <31568819+umeshdhanwal@users.noreply.github.com>
2017-09-30 09:23:19 +0800

    Update Final_Google_alerts.py

--- Final_Google_alerts.py
+++ Final_Google_alerts.py
@@ -1,8 +1,11 @@
-from git import Repo
 import feedparser
-import git
+#import git
 import os
 #import subprocess
+import dropbox
+
+##Pull the latest file from git
+print subprocess.check_output('git pull origin master', shell=True)
 
 repo_dir = 'GoogleAlerts'
 URL_feed='https://www.google.com/alerts/feeds/03052694921060148104/10484262279899711572'
@@ -19,17 +22,19 @@ for e in d.entries:
      #file.write(str(e.description))
      file.write("\n") # 2 newlines
 
-file.close     
+file.close
+
+app_key = 'ez341m6npdgliwh'
+app_secret = 'aYGqbBWFEzoAAAAAAAAADibvdDcTby6Pjgc8Bl4nZc4PASuecEG9isKWkWcd44o4'
+
+flow = dropbox.client.DropboxOAuth2FlowNoRedirect(app_key, app_secret)
+
+f = open('testfile.txt', 'rb')
+response = client.put_file('/magnum-opus.txt', f)
+print "uploaded:", response
 
 #Reading the Pdf already stored on local drive and
 #then reading it for writing
-repo = git.Repo(os.getcwd()
-files = repo.git.diff(None, name_only=True)
-for f in files.split('\n'):
-    show_diff(f)
-    repo.git.add(f)
-
-repo.git.commit('-m', 'test commit', author='umeshdhanwal@gmail.com')
 
 #print subprocess.check_output('git add .', shell=True)
 #print subprocess.check_output('git commit -m "This"', shell=True)
