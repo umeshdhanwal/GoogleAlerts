8f1c002b12616633f60d03e1724e869a83731949
umeshdhanwal <31568819+umeshdhanwal@users.noreply.github.com>
2017-09-30 14:21:48 +0800

    Update Final_Google_alerts.py

--- Final_Google_alerts.py
+++ Final_Google_alerts.py
@@ -45,17 +45,27 @@ file.close
 
 #Drop box keys     
 app_key = 'ez341m6npdgliwh'
-app_secret = 'aYGqbBWFEzoAAAAAAAAADibvdDcTby6Pjgc8Bl4nZc4PASuecEG9isKWkWcd44o4'
+access_token = 'aYGqbBWFEzoAAAAAAAAADibvdDcTby6Pjgc8Bl4nZc4PASuecEG9isKWkWcd44o4'
 
 dbx = dropbox.Dropbox(app_secret)
 
-print(os.getcwd())
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
 
-filetocopy=open(NameofFile,'r')
+    def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+         
 print("The copiedName:",NameofFile)
-data = filetocopy.read()      # copy to a string
-print(data)
 
-dbx.files_upload('This is the one I was talking', '/'+NameofFile, mute=True)
+file_from=NameofFile
+file_to="\"+NameofFile
+
+transferData.upload_file(file_from, file_to)
 print(dbx.files_get_metadata( '/'+NameofFile).server_modified)
 filetocopy.close()
