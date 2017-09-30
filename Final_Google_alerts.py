0aad202aba63b621224060b47a84a5a46e172fec
umeshdhanwal <31568819+umeshdhanwal@users.noreply.github.com>
2017-09-30 14:58:53 +0800

    Update Final_Google_alerts.py

--- Final_Google_alerts.py
+++ Final_Google_alerts.py
@@ -50,8 +50,8 @@ access_token = 'aYGqbBWFEzoAAAAAAAAADibvdDcTby6Pjgc8Bl4nZc4PASuecEG9isKWkWcd44o4
 dbx = dropbox.Dropbox(access_token)
 
 def upload_file(self, file_from, file_to):
-"""upload a file to Dropbox using API v2
- """
+##upload a file to Dropbox using API v2
+ 
         dbx = dropbox.Dropbox(self.access_token)
              with open(file_from, 'rb') as f:
                  dbx.files_upload(f.read(), file_to)
