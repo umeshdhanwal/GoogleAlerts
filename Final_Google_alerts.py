b174510a729532731b0348e26b06beecc693a4c7
umeshdhanwal <31568819+umeshdhanwal@users.noreply.github.com>
2017-09-30 14:59:33 +0800

    Update Final_Google_alerts.py

--- Final_Google_alerts.py
+++ Final_Google_alerts.py
@@ -53,7 +53,7 @@ def upload_file(self, file_from, file_to):
 ##upload a file to Dropbox using API v2
  
         dbx = dropbox.Dropbox(self.access_token)
-             with open(file_from, 'rb') as f:
+        with open(file_from, 'rb') as f:
                  dbx.files_upload(f.read(), file_to)
          
 print("The copiedName:",NameofFile)
