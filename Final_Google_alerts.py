fb550beffa6a920d367c4cca9e62d3edd7cc493b
umeshdhanwal <31568819+umeshdhanwal@users.noreply.github.com>
2017-10-05 11:25:22 +0800

    Update Final_Google_alerts.py

--- Final_Google_alerts.py
+++ Final_Google_alerts.py
@@ -13,9 +13,12 @@ import subprocess
 import dropbox
 import unicodedata
 import datetime
+from weasyprint import HTML, CSS
 
-now = datetime.datetime.now()
-now=now.strftime("%Y-%m-%d:%H:%M")
+
+#Defining the time of txt file
+now = datetime.datetime.now().strftime("%Y-%m-%d:%H:%M")
+
 
 ##Pull the latest file from git
 #subprocess.check_output('git reset --hard', shell=True)
@@ -34,20 +37,38 @@ if 'google' in URL_feed:
 
 d = feedparser.parse(URL_feed)
 
-#Create Directory for the files:
-script_dir = os.path.dirname(os.path.abspath(__file__))
-dest_dir = os.path.join(script_dir, 'News_Alerts')
-try:
-    os.makedirs(dest_dir)
-except OSError:
-    pass # already exists
+#Trying to convert to pdf
+url='https://www.irishtimes.com/news/politics/liam-cosgrave-former-taoiseach-and-fine-gael-leader-dies-aged-97-1.3244509'
 
+if "irishtimes" in url:
+   pdf = HTML(url).write_pdf('google.pdf',stylesheets=[CSS(string='@page { size: 26.9cm 300cm; margin: 0in 0in 0in 0in};'
+           '@media print { nav { display: none; } }')])
+else:
+    pdf=HTML(url).write_pdf('google.pdf')
+
+
+#pdf = HTML(url).write_pdf('google.pdf',stylesheets=[CSS(string='@page {size: 27cm 300cm;margin: 0in 0.44in 0.2in 0.44in;}')])
+#open('google.pdf', 'w').write(pdf)
+#print(url.text)
+
+
+#Create Directory for the files:
+def createdir(value):
+   script_dir = os.path.dirname(os.path.abspath(__file__))
+   dest_dir = os.path.join(script_dir, value)
+   try:
+      os.makedirs(dest_dir)
+   except OSError:
+      pass # already exists
+   return dest_dir
+
+dest_dir=createdir('News_Alerts')
 
 #Write to  txt files and then use it for conversion
 NameofFile='Google_Alerts_'+now+'.txt'
 print("The startingName:",NameofFile)
 
-os.chdir('/home/umeshlives/GoogleAlerts')
+os.chdir('/home/umeshdhanwal/GoogleAlerts')
 
 path = os.path.join(dest_dir, NameofFile)
 file = open(path,"w")
@@ -64,7 +85,6 @@ for e in d.entries:
 file.close
 
 #Drop box keys
-app_key = 'ez341m6npdgliwh'
 #access_token = 'o2HubcOnjaAAAAAAAACGENE_EX_fTQ2h4dErQe0yjEO6jfRgtSiLjfFi-SvmZ67N'
 access_token = 'aYGqbBWFEzoAAAAAAAAADibvdDcTby6Pjgc8Bl4nZc4PASuecEG9isKWkWcd44o4'
 
@@ -73,16 +93,19 @@ rootdir = 'News_Alerts'
 
 print ("Attempting to upload...")
 # walk return first the current folder that it walk, then tuples of dirs and files not "subdir, dirs, files"
-for dir, dirs, files in os.walk(rootdir):
-    for file in files:
-        try:
-            #file_path = os.path.join(dir, file)
-            file_path = os.path.join(dir, file)
-            dest_path = os.path.join('/GoogleAlerts', file)
-            print('Uploading %s to %s' % (file_path, dest_path))
-            with open(file_path,'r') as f:
+def uploadfile(rootdir):
+   for dir, dirs, files in os.walk(rootdir):
+       for file in files:
+          try:
+             #file_path = os.path.join(dir, file)
+             file_path = os.path.join(dir, file)
+             dest_path = os.path.join('/GoogleAlerts',rootdir,file)
+             print('Uploading %s to %s' % (file_path, dest_path))
+             with open(file_path,'r') as f:
                 dbx.files_upload(f.read(), dest_path, mute=True)
-        except Exception as err:
-            print("Failed to upload %s\n%s" % (file, err))
+          except Exception as err:
+             print("Failed to upload %s\n%s" % (file, err))
 
+#Uploading the files
+uploadfile(rootdir)
 print("Finished upload.")
