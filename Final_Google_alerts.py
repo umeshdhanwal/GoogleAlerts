a36a8f9b77089f3da16f6398dd4e67df8f4c2432
umeshdhanwal <31568819+umeshdhanwal@users.noreply.github.com>
2017-10-06 14:56:29 +0800

    Update Final_Google_alerts.py

--- Final_Google_alerts.py
+++ Final_Google_alerts.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 30 09:05:15 2017
+The Below code would output the Google Alerts feed to the dropbox specified
+in the access token and it would generate pdf file for it in another folder
+
+Refer to the Fourth line of the alert for identifying the location of the saved pdf file in the text file
+
+Created on Fri Oct 30 09:05:15 2017
 
 @author: Hsemu
 """
-import warnings
 
 #Muting the warnings
+import warnings
 warnings.filterwarnings("ignore")
 
-#from git import Repo
 import feedparser
-#import git
 import os
 import subprocess
 import dropbox
@@ -21,25 +24,42 @@ from weasyprint import HTML, CSS
 import re
 import requests, lxml.html
 
+'''Please put the URL feed of the Google alerts which you want to feed'''
+#URL_feed='https://www.google.com/alerts/feeds/03052694921060148104/10484262279899711572' #Alert for Donald Trump
+URL_feed='https://www.google.ie/alerts/feeds/06782147434896058293/17777381263490192727' #Alert for Ryan Air
+#URL_feed='https://www.google.ie/alerts/feeds/06782147434896058293/861461405236497894'  #Alert for Las Vegas
+#URL_feed='https://www.google.com/alerts/feeds/06782147434896058293/8909055318872650373' #Alert for Trump
+#URL_feed='https://www.google.com/alerts/feeds/14273445301609542014/8457109545342134256' #Alert for Itishtimes opinion of Newton Emerson
 
-os.chdir('/home/umeshdhanwal/GoogleAlerts')
+'''Please provide the username and password for Irishtimes'''
+user='a.m.connolly@idiro.com'
+passwd='Umesh123'
 
+'''Here input the values which needs to be created for the home directory'''
+os.chdir('/home/umeshdhanwal/GoogleAlerts') #Input the local directory to be created for storing the alerts
+repo_dir = 'GoogleAlerts' #Directory for defining the starting folder for parsing to dropbox
+txt_alert_dir='News_Alerts' #input the name of directory for storing text file
+pdf_alert_dir='News_Alerts_pdf' #input the name of directory for storing pdf file
+
+'''Putting the Drop box access token for running'''
+#access_token = 'o2HubcOnjaAAAAAAAACGENE_EX_fTQ2h4dErQe0yjEO6jfRgtSiLjfFi-SvmZ67N'
+access_token = 'aYGqbBWFEzoAAAAAAAAADibvdDcTby6Pjgc8Bl4nZc4PASuecEG9isKWkWcd44o4'
 
 #Login for the website
 def loginsite(nameofsite,username,password):
     s = requests.session()
     details = {
-    'signin__email': 'a.m.connolly@idiro.com',
-    'signin__password': 'Umesh123'
+    'signin__email': username,
+    'signin__password': password
     }
 
     response = s.post(nameofsite, data=details)
     print "Logged in NewSite:",response.url
     return s
 
-site=loginsite('https://www.irishtimes.com/','a.m.connolly@idiro.com','Umesh123')
+site=loginsite('https://www.irishtimes.com/',user,passwd)
 
-##Replace string in text
+#Replace string in text
 def replace_all(text, dic):
     for i, j in dic.iteritems():
         text = text.replace(i, j)
@@ -58,24 +78,15 @@ def createdir(value):
       pass # already exists
    return dest_dir
 
-dest_dir=createdir('News_Alerts')
-dest_dir_pdf=createdir('News_Alerts_pdf')
-
-
+dest_dir=createdir(txt_alert_dir)
+dest_dir_pdf=createdir(pdf_alert_dir)
 
 #Write to  txt files and then use it for conversion
-NameofFile='Google_Alerts_'+now+'.txt'
+NameofFile=repo_dir+'_'+now+'.txt'
 print("The Name of alert file created just now:",NameofFile)
 
 path = os.path.join(dest_dir, NameofFile)
 file = open(path,"w")
-
-repo_dir = 'GoogleAlerts'
-#URL_feed='https://www.google.com/alerts/feeds/03052694921060148104/10484262279899711572'
-#URL_feed='https://www.google.ie/alerts/feeds/06782147434896058293/17777381263490192727'
-#URL_feed='https://www.google.ie/alerts/feeds/06782147434896058293/861461405236497894'
-#URL_feed='https://www.google.com/alerts/feeds/06782147434896058293/8909055318872650373'
-URL_feed='https://www.google.com/alerts/feeds/14273445301609542014/8457109545342134256'
 
 if 'google' in URL_feed:
     URL_feed=URL_feed.replace('google.ie', 'google.com')
@@ -103,19 +114,12 @@ for i,e in enumerate(d.entries):
        file.write("Location of file in PythonAnywhere:%s"%pdffilename+"\n")
        file.write("\n") # 2 newlines
      except:
-       pass
+         pass
 
 file.close
-
-
-##Pull the latest file from git
-#subprocess.check_output('git reset --hard', shell=True)
-#subprocess.check_output('git clean -df', shell=True)
-#subprocess.check_output('git stash', shell=True)
-#subprocess.check_output('git pull origin master', shell=True)
+file.flush()
 
 #Trying to convert to pdf
-#url='https://stackoverflow.com/questions/30292625/weasyprint-html-to-pdf-huge-gap-in-right-margin'
 print(url)
 for i in range(len(url)):
     try:
@@ -131,18 +135,8 @@ for i in range(len(url)):
     except:
         pass
 
-
-#pdf = HTML(url).write_pdf('google.pdf',stylesheets=[CSS(string='@page {size: 27cm 300cm;margin: 0in 0.44in 0.2in 0.44in;}')])
-#open('google.pdf', 'w').write(pdf)
-#print(url.text)
-
-#Drop box keys
-#access_token = 'o2HubcOnjaAAAAAAAACGENE_EX_fTQ2h4dErQe0yjEO6jfRgtSiLjfFi-SvmZ67N'
-access_token = 'aYGqbBWFEzoAAAAAAAAADibvdDcTby6Pjgc8Bl4nZc4PASuecEG9isKWkWcd44o4'
-
+#Accesing the dropbox
 dbx = dropbox.Dropbox(access_token)
-rootdir = 'News_Alerts'
-root_pdfdir='News_Alerts_pdf'
 
 print ("Attempting to upload...")
 # walk return first the current folder that it walk, then tuples of dirs and files not "subdir, dirs, files"
@@ -152,7 +146,7 @@ def uploadfile(rootdir):
           try:
              #file_path = os.path.join(dir, file)
              file_path = os.path.join(dir, file)
-             dest_path = os.path.join('/GoogleAlerts',rootdir,file)
+             dest_path = os.path.join('/'+repo_dir,rootdir,file)
              print('Uploading %s to %s' % (file_path, dest_path))
              with open(file_path,'r') as f:
                 dbx.files_upload(f.read(), dest_path, mode=dropbox.files.WriteMode.overwrite)
@@ -160,7 +154,7 @@ def uploadfile(rootdir):
              print("Failed to upload %s\n%s" % (file, err))
 
 #Uploading the files
-uploadfile(rootdir)
-uploadfile(root_pdfdir)
+uploadfile(pdf_alert_dir)
+uploadfile(txt_alert_dir)
 
 print("Finished upload.")
